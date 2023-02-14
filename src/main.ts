f98929f52d457ade9fd45517720eb57ae64ea987
Francisco Dany <dany.francisco@outlook.com>
2023-02-14 22:51:13 +0100

    add script to build using typescript present in node_modules

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { API, PlatformAccessory } from "homebridge";
 // import { EsgiDummyAccessory } from "./esgi-dummy.accessory";
 import { EsgiDummyAccessory1 } from "./esgi-dummy.accessory-1";
-// import express from 'express';
+import express from 'express';
 
 export default (api: API) => {
     // api.registerAccessory('DummyESGI', EsgiDummyAccessory);
     api.registerAccessory('DummyESGI', EsgiDummyAccessory1);
 
-    /*api.on('didFinishLaunching', () => {
+    api.on('didFinishLaunching', () => {
         const app = express()
 
         app.get('/', (req, res) => {
@@ -22,5 +22,5 @@ export default (api: API) => {
         });
 
         app.listen(4000, () => console.log('Listen on port 4000'));
-    });*/
+    });
 }
